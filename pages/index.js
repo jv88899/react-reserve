aab97915434120ffe340167f22c94b38efdaf9da
jv88899 <jv88899@gmail.com>
2021-07-05 14:19:26 -0500

    converted from getting data on the client to getting data from server

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import axios from "axios";
 
-function Home() {
-  React.useEffect(() => {
-    getProducts();
-  }, []);
-
-  async function getProducts() {
-    const url = "http://localhost:3000/api/products";
-    const response = await axios.get(url);
-    console.log(response.data);
-  }
-
+function Home({ products }) {
   return <>home</>;
 }
+
+Home.getInitialProps = async () => {
+  const url = "http://localhost:3000/api/products";
+  const response = await axios.get(url);
+  return { products: response.data };
+};
 
 export default Home;
