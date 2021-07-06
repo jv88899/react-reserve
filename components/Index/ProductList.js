5336b014dca4420e5431b3ea3df82ff41d546534
jv88899 <jv88899@gmail.com>
2021-07-06 16:08:49 -0500

    added product price to product card

--- components/Index/ProductList.js
+++ components/Index/ProductList.js
@@ -5,6 +5,7 @@ function ProductList({ products }) {
     return products.map((product) => ({
       header: product.name,
       image: product.mediaUrl,
+      meta: `$${product.price}`,
       color: "teal",
       fluid: true,
       childKey: product._id,
