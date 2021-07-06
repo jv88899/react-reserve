f9367547b83be6171fdb0e16eabf31e709e4e859
jv88899 <jv88899@gmail.com>
2021-07-06 17:19:12 -0500

    set up modal and modal functionality in ProductAttributes

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,6 +1,15 @@
-import { Button, Header } from "semantic-ui-react";
+import React from "react";
+import {
+  Button,
+  Header,
+  Modal,
+  ModalActions,
+  ModalContent,
+  ModalHeader,
+} from "semantic-ui-react";
 
 function ProductAttributes({ description }) {
+  const [modal, setModal] = React.useState(false);
   return (
     <>
       <Header as="h3">About this product</Header>
@@ -9,7 +18,24 @@ function ProductAttributes({ description }) {
         icon="trash alternate outline"
         color="red"
         content="Delete Product"
+        onClick={() => setModal(true)}
       />
+      <Modal open={modal} dimmer="blurring">
+        <ModalHeader>Confirm Delete</ModalHeader>
+        <ModalContent>
+          <p>Are you sure you want to delete this product?</p>
+        </ModalContent>
+        <ModalActions>
+          <Button content="Cancel" onClick={() => setModal(false)} />
+          <Button
+            negative
+            icon="trash"
+            labelPosition="right"
+            content="Delete"
+            onClick={() => setModal(false)}
+          />
+        </ModalActions>
+      </Modal>
     </>
   );
 }
