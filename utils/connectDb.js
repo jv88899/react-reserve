d11d7da65ef21e33ed6d018efa1539d7f17adfa5
jv88899 <jv88899@gmail.com>
2021-07-06 15:38:19 -0500

    connected database and now pull product data from database

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -3,14 +3,15 @@ const connection = {};
 
 async function connectDb() {
   if (connection.isConnected) {
+    // Use existing database connection
     console.log("Using existing connection");
     return;
   }
-
+  // Use new database connection
   const db = await mongoose.connect(process.env.MONGO_SRV, {
     useCreateIndex: true,
     useFindAndModify: false,
-    useNewUrlParster: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   });
   console.log("DB Connected");
