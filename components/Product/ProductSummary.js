0488da8e169a7b1486e3beaa852146ae81be1de4
jv88899 <jv88899@gmail.com>
2021-07-06 16:38:23 -0500

    added content to ProductSummary component

--- components/Product/ProductSummary.js
+++ components/Product/ProductSummary.js
@@ -1,5 +1,33 @@
-function ProductSummary() {
-  return <>ProductSummary</>;
+import {
+  Item,
+  ItemContent,
+  ItemDescription,
+  ItemExtra,
+  ItemGroup,
+  ItemHeader,
+  ItemImage,
+  Label,
+} from "semantic-ui-react";
+import AddProductToCart from "./AddProductToCart";
+
+function ProductSummary({ name, mediaUrl, _id, price, sku }) {
+  return (
+    <ItemGroup>
+      <Item>
+        <ItemImage size="medium" src={mediaUrl} />
+        <ItemContent>
+          <ItemHeader>{name}</ItemHeader>
+          <ItemDescription>
+            <p>${price}</p>
+            <Label>SKU: {sku}</Label>
+          </ItemDescription>
+          <ItemExtra>
+            <AddProductToCart productId={_id} />
+          </ItemExtra>
+        </ItemContent>
+      </Item>
+    </ItemGroup>
+  );
 }
 
 export default ProductSummary;
