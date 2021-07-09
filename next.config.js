5bf59f9e5065265b71d7ef3d019e34c1435b877c
jv88899 <jv88899@gmail.com>
2021-07-09 09:20:12 -0500

    added functionality to enable users to sign up for an account

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     MONGO_SRV:
       "mongodb+srv://Admin:eBHhXgVKcgnjNo1P@reactreserve.wdooo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET: "<insert-jwt-secret>",
+    JWT_SECRET: "afsklj;asfjlkasdfkljfs",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dweimb3da/image/upload",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>",
   },
--- pages/signup.js
+++ pages/signup.js
@@ -8,7 +8,10 @@ import {
   Segment,
 } from "semantic-ui-react";
 import Link from "next/link";
+import axios from "axios";
 import catchErrors from "../utils/catchErrors";
+import baseUrl from "../utils/baseUrl";
+import { handleLogin } from "../utils/auth";
 
 const INITIAL_USER = {
   name: "",
@@ -37,7 +40,10 @@ function Signup() {
     try {
       setLoading(true);
       setError("");
-      console.log(user);
+      const url = `${baseUrl}/api/signup`;
+      const payload = { ...user };
+      const response = await axios.post(url, payload);
+      handleLogin(response.data);
     } catch (error) {
       catchErrors(error, setError);
     } finally {
