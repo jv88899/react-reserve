8beff431744eec24df2aafc6c34d1f27ae849040
jv88899 <jv88899@gmail.com>
2021-07-10 11:38:36 -0500

    added functionality to login

--- pages/login.js
+++ pages/login.js
@@ -9,6 +9,9 @@ import {
 } from "semantic-ui-react";
 import Link from "next/link";
 import catchErrors from "../utils/catchErrors";
+import baseUrl from "../utils/baseUrl";
+import axios from "axios";
+import { handleLogin } from "../utils/auth";
 
 const INITIAL_USER = {
   email: "",
@@ -36,7 +39,10 @@ function Login() {
     try {
       setLoading(true);
       setError("");
-      console.log(user);
+      const url = `${baseUrl}/api/login`;
+      const payload = { ...user };
+      const response = await axios.post(url, payload);
+      handleLogin(response.data);
     } catch (error) {
       catchErrors(error, setError);
     } finally {
