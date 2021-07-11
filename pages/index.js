06dd7dac2ebd68224de33ea69ad6618ad2bbf79c
jv88899 <jv88899@gmail.com>
2021-07-11 18:51:43 -0500

    updated products api to add pagination functionality

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,12 @@ function Home({ products }) {
   return <ProductList products={products} />;
 }
 
-Home.getInitialProps = async () => {
+Home.getInitialProps = async (ctx) => {
+  const page = ctx.query.page ? ctx.query.page : "1";
+  const size = 9;
   const url = `${baseUrl}/api/products`;
-  const response = await axios.get(url);
+  const payload = { params: { page, size } };
+  const response = await axios.get(url, payload);
   return { products: response.data };
 };
 
