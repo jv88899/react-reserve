8e6eeb68adc506d7d649491110a67c353f4b2a06
jv88899 <jv88899@gmail.com>
2021-07-11 16:09:22 -0500

    set up CartSummary component

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,12 +1,25 @@
+import React from "react";
 import { Button, Divider, Segment } from "semantic-ui-react";
 
-function CartSummary() {
+function CartSummary({ products }) {
+  const [isCartEmpty, setCartEmpty] = React.useState(false);
+
+  React.useEffect(() => {
+    setCartEmpty(products.length === 0);
+  }, [products]);
+
   return (
     <>
       <Divider />
       <Segment clearing size="large">
         <strong>Sub total:</strong> $0.00
-        <Button icon="cart" color="teal" floated="right" content="Checkout" />
+        <Button
+          icon="cart"
+          disabled={isCartEmpty}
+          color="teal"
+          floated="right"
+          content="Checkout"
+        />
       </Segment>
     </>
   );
