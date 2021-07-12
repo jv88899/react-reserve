53ef0c47f74c609f3b91f1854a5ce1c699a58dab
jv88899 <jv88899@gmail.com>
2021-07-11 19:39:57 -0500

    set up basic structure for AccountHeader

--- components/Account/AccountHeader.js
+++ components/Account/AccountHeader.js
@@ -1,5 +1,30 @@
-function AccountHeader() {
-  return <>AccountHeader</>;
+import {
+  Header,
+  HeaderSubheader,
+  Icon,
+  Label,
+  Segment,
+} from "semantic-ui-react";
+
+function AccountHeader({ role, email, name, createdAt }) {
+  return (
+    <Segment secondary inverted color="violet">
+      <Label
+        color="teal"
+        size="large"
+        ribbon
+        icon="privacy"
+        style={{ textTransform: "capitalize" }}
+        content={role}
+      />
+      <Header inverted textAlign="center" as="h1" icon>
+        <Icon name="user" />
+        {name}
+        <HeaderSubheader>{email}</HeaderSubheader>
+        <HeaderSubheader>Joined {createdAt}</HeaderSubheader>
+      </Header>
+    </Segment>
+  );
 }
 
 export default AccountHeader;
