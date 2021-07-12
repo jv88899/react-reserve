9e2e75cb533ee9009cdcc821c7ab065726f1f690
jv88899 <jv88899@gmail.com>
2021-07-12 13:43:55 -0500

    updated baseUrl utility function with correct production link

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://reactreserve-teal.vercel.app"
+    ? "https://reactreserve-jv88899.vercel.app/"
     : "http://localhost:3000";
 
 export default baseUrl;
