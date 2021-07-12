a475473382f49918e23b480e3a4a36ad042126b8
jv88899 <jv88899@gmail.com>
2021-07-12 10:28:52 -0500

    created user table in AccountPermissions

--- components/Account/AccountPermissions.js
+++ components/Account/AccountPermissions.js
@@ -1,5 +1,75 @@
+import React from "react";
+import axios from "axios";
+import cookie from "js-cookie";
+import baseUrl from "../../utils/baseUrl";
+import {
+  Checkbox,
+  Header,
+  Icon,
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableHeaderCell,
+  TableRow,
+} from "semantic-ui-react";
+
 function AccountPermissions() {
-  return <>AccountPermissions</>;
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+    getUsers();
+  }, []);
+
+  async function getUsers() {
+    const url = `${baseUrl}/api/users`;
+    const token = cookie.get("token");
+    const payload = { headers: { Authorization: token } };
+    const response = await axios.get(url, payload);
+    setUsers(response.data);
+  }
+
+  return (
+    <div style={{ margin: "2em 0" }}>
+      <Header as="h2">
+        <Icon name="settings" />
+        User Permissions
+      </Header>
+      <Table compact celled definition>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderCell />
+            <TableHeaderCell>Name</TableHeaderCell>
+            <TableHeaderCell>Email</TableHeaderCell>
+            <TableHeaderCell>Joined</TableHeaderCell>
+            <TableHeaderCell>Updated</TableHeaderCell>
+            <TableHeaderCell>Role</TableHeaderCell>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {users.map((user) => (
+            <UserPermission key={user._id} user={user} />
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
+
+function UserPermission({ user }) {
+  return (
+    <TableRow>
+      <TableCell collapsing>
+        <Checkbox toggle />
+      </TableCell>
+      <TableCell>{user.name}</TableCell>
+      <TableCell>{user.email}</TableCell>
+      <TableCell>{user.createdAt}</TableCell>
+      <TableCell>{user.updatedAt}</TableCell>
+      <TableCell>{user.role}</TableCell>
+    </TableRow>
+  );
 }
 
 export default AccountPermissions;
